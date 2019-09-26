5825effa7c74fdd7fedf5f4e83a3a7e395294c29
darkalex001 <244786317@qq.com>
2019-09-26 21:25:11 +0800

    客户端发送数据的时候服务端也可以收到数据

--- socketServer/client.js
+++ socketServer/client.js
@@ -1,12 +1,14 @@
 const net = require('net');
-const client = net.createConnection({ port: 1337 }, () => {
+var hostname = process.argv[2];
+var port = process.argv[3];
+const client = net.createConnection({host:hostname,port:port}, () => {
   // 'connect' 监听器
   console.log('已连接到服务器');
   client.write('你好世界!\r\n');
 });
 client.on('data', (data) => {
   console.log(data.toString());
-  client.end();
+  //client.end();
 });
 client.on('end', () => {
   console.log('已从服务器断开');
--- socketServer/server.js
+++ socketServer/server.js
@@ -1,8 +1,15 @@
 var net = require('net');
 
 var server = net.createServer(function (socket) {
-    socket.write('Echo server\r\n');
-    socket.pipe(socket);
+    socket.write('success! \r\n');
+    //socket.pipe(socket);
+    socket.on('data',function(data){
+	console.log(data.toString());
+        socket.write(data);
+    });
+    socket.on('end',function(){
+        socket.write("end!");
+    });
 });
 
 server.listen(1337,'127.0.0.1');
