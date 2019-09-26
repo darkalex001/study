2a7e77960ef08a547bf4b67127e75ab7e74caf17
darkalex001 <244786317@qq.com>
2019-09-26 22:11:13 +0800

    node.js实时数据通讯演示完毕

--- socketServer/client.js
+++ socketServer/client.js
@@ -4,7 +4,16 @@ var port = process.argv[3];
 const client = net.createConnection({host:hostname,port:port}, () => {
   // 'connect' 监听器
   console.log('已连接到服务器');
-  client.write('你好世界!\r\n');
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('readable', () => {
+    let chunk;
+    // 使用循环确保我们读取所有的可用数据。
+    while ((chunk = process.stdin.read()) !== null) {
+      client.write(`数据: ${chunk}`);
+    }
+  });
+  
 });
 client.on('data', (data) => {
   console.log(data.toString());
